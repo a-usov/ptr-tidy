d918ea9d8a18836f380f2b6f4ceb9f06e197482d
Artem Usov <artem.o.u@gmail.com>
2020-10-11 04:43:12 +0100

    Finish ClangAST tutorial program

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/Tooling/Tooling.h"
+
+using namespace llvm;
+using namespace clang;
+using namespace clang::tooling;
+using namespace clang::ast_matchers;
+
+class DumpCallback : public MatchFinder::MatchCallback {
+	virtual void run(const MatchFinder::MatchResult &Result) {
+		llvm::errs() << "---\n";
+		Result.Nodes.getNodeAs<CXXRecordDecl>("x")->dump();
+	}
+};
 
 int main() {
 	std::cout << "Hello world" << std::endl;
-}
 
-class DumpCallback : public clang::ast_matchers::MatchFinder::MatchCallback {};
+	DumpCallback Callback;
+	MatchFinder Finder;
+	Finder.addMatcher(recordDecl().bind("x"), &Callback);
+	std::unique_ptr<FrontendActionFactory> Factory(newFrontendActionFactory(&Finder));
+	clang::tooling::runToolOnCode(Factory->create(), "class X {};");
+}
