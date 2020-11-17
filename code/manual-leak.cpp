31e1bb9d1c8a4998a07a765c22eee7a5bb30db60
Artem Usov <artem.o.u@gmail.com>
2020-11-17 02:20:04 +0000

    finish draft problem statement, start background

---
+++ code/manual-leak.cpp
@@ -0,0 +1,8 @@
+void foo(int x) {
+    Shape *p = new Circle{Point{0,0},10};
+    // ...
+    if (x<0) throw Bad_x{}; // potential leak
+    if (x==0) return; // potential leak
+    // ...
+    delete p;
+}
