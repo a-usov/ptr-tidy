d9348683a1b5921ca5e471834d6606cf94934ee2
Artem Usov <artem.o.u@gmail.com>
2021-02-19 00:44:38 +0000

    Big Rewrite

--- src/Callback.h
+++ src/Callback.h
@@ -4,15 +4,21 @@
 #include "PtrRewriter.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 
-class CountCallback : public clang::ast_matchers::MatchFinder::MatchCallback {
-private:
-  IrHandler m_irHandler;
+class Callback : public clang::ast_matchers::MatchFinder::MatchCallback {
   PtrRewriter m_ptrRewriter;
+  const std::unique_ptr<llvm::Module> m_module;
+  // fine to store since we know it will life and wont be reallocated - stores file path to source code
+  const llvm::StringRef m_path;
 
 public:
-  explicit CountCallback(const llvm::StringRef codePath) : m_irHandler(codePath){};
+  Callback(clang::tooling::ClangTool &tool, std::unique_ptr<llvm::Module> module, const llvm::StringRef path)
+      : m_ptrRewriter(tool), m_module(std::move(module)), m_path(path){};
 
   void run(const clang::ast_matchers::MatchFinder::MatchResult &result) override;
 
-  clang::Rewriter &getRewriter() { return m_ptrRewriter.getRewriter(); }
+  const clang::Rewriter &getRewriter() { return m_ptrRewriter.getRewriter(); }
 };
+
+std::string getFunctionMangledName(clang::ASTContext *context, const clang::FunctionDecl *namedDecl);
+const llvm::Value *getIRValue(const clang::VarDecl *var, const llvm::Module *module, clang::ASTContext *context);
+bool checkVarValid(const clang::VarDecl *var);
--- src/Helper.h
+++ src/Helper.h
@@ -1,45 +1,6 @@
 #pragma once
 
-#include "clang/AST/ASTContext.h"
-#include "clang/AST/Mangle.h"
-#include "clang/Lex/Lexer.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/ValueSymbolTable.h"
-#include "llvm/Support/MemoryBuffer.h"
-
-inline void exit(const llvm::StringRef &message) {
+inline void exit(const llvm::Twine &message) {
   llvm::errs() << message << "\n";
   std::exit(-1);
-}
-
-inline std::string getMangledName(clang::ASTContext *context, const clang::NamedDecl *namedDecl) {
-  auto mangler = context->createMangleContext(&context->getTargetInfo());
-
-  if (mangler->shouldMangleDeclName(namedDecl)) {
-    std::string mangledName;
-    llvm::raw_string_ostream mangledNameStream(mangledName);
-    mangler->mangleName(namedDecl, mangledNameStream);
-    mangledNameStream.flush();
-
-    return mangledName;
-  } else {
-    return namedDecl->getNameAsString();
-  }
-}
-
-inline llvm::Value *getLocalValue(const clang::VarDecl *var, const llvm::Module &module, clang::ASTContext *context) {
-  auto parentFunctionAST = llvm::dyn_cast<clang::FunctionDecl>(var->getParentFunctionOrMethod());
-  auto parentFunctionIR = module.getFunction(getMangledName(context, parentFunctionAST));
-  auto valueSymbol = parentFunctionIR->getValueSymbolTable();
-  return valueSymbol->lookup(var->getName());
-}
-
-inline std::optional<std::unique_ptr<llvm::MemoryBuffer>> getFile(const llvm::StringRef name) {
-  auto sourceFileOrError = llvm::MemoryBuffer::getFile(name);
-  return sourceFileOrError ? std::make_optional(std::move(sourceFileOrError.get())) : std::nullopt;
-}
-
-inline llvm::StringRef strFromSourceRange(clang::SourceRange sourceRange, const clang::SourceManager &sm,
-                                          const clang::LangOptions &lo) {
-  return clang::Lexer::getSourceText(clang::Lexer::getAsCharRange(sourceRange, sm, lo), sm, lo);
 }
