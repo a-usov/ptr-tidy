9421e07b5c4bfb03d79fdf7b1709667bfa11645b
Artem Usov <artem.o.u@gmail.com>
2021-03-01 22:06:52 +0000

    Fix rewriting with classes and class initialisers

--- test/testPtrRewriter.t.cpp
+++ test/testPtrRewriter.t.cpp
@@ -3,7 +3,6 @@
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "gtest/gtest.h"
 
-using namespace clang::ast_matchers;
 using namespace clang::tooling;
 
 // returns empty string if input code was unchanged
@@ -24,24 +23,14 @@ std::string compareRewriterOutput(clang::Rewriter &rewriter) {
   return output;
 }
 
-class LambdaCallback : public MatchFinder::MatchCallback {
-  std::function<void(const MatchFinder::MatchResult &result)> m_lambda;
-
-  void run(const MatchFinder::MatchResult &result) override { m_lambda(result); }
-
-public:
-  explicit LambdaCallback(std::function<void(const MatchFinder::MatchResult &result)> lambda)
-      : m_lambda(std::move(lambda)){};
-};
-
 class PtrRewriterTest : public ::testing::Test {
 protected:
   TestTool tool;
   PtrRewriter<TestTool> rewriter{tool};
   std::unique_ptr<FrontendActionFactory> factory;
-  MatchFinder finder;
+  clang::ast_matchers::MatchFinder finder;
 
-  LambdaCallback callback{[&](const MatchFinder::MatchResult &result) {
+  LambdaCallback callback{[&](const clang::ast_matchers::MatchFinder::MatchResult &result) {
     rewriter.initialise(*result.SourceManager, result.Context->getLangOpts());
 
     auto var = result.Nodes.getNodeAs<clang::VarDecl>("");
@@ -53,7 +42,7 @@ protected:
   }};
 
   PtrRewriterTest() {
-    finder.addMatcher(varDecl().bind(""), &callback);
+    finder.addMatcher(clang::ast_matchers::varDecl().bind(""), &callback);
     factory = newFrontendActionFactory(&finder);
   }
 };
@@ -112,8 +101,16 @@ TEST_F(PtrRewriterTest, NonPointerVariableDeclarationTypeUnChanged) {
   ASSERT_EQ(compareRewriterOutput(rewriter.getRewriter()), "");
 }
 
-TEST_F(PtrRewriterTest, InitialisationNewChanged) {
+TEST_F(PtrRewriterTest, CallInitialisationNewChanged) {
   tool.code = "void test(){int *a = new int(2);}";
+
+  runToolOnCode(factory->create(), tool.code);
+  ASSERT_EQ(compareRewriterOutput(rewriter.getRewriter()),
+            "void test(){std::shared_ptr<int> a = std::make_shared<int>(2);}");
+}
+
+TEST_F(PtrRewriterTest, ListInitialisationNewChanged) {
+  tool.code = "void test(){int *a = new int{2};}";
 
   runToolOnCode(factory->create(), tool.code);
   ASSERT_EQ(compareRewriterOutput(rewriter.getRewriter()),
@@ -160,4 +157,54 @@ TEST_F(PtrRewriterTest, HandleArrays) {
   runToolOnCode(factory->create(), tool.code);
   ASSERT_EQ(compareRewriterOutput(rewriter.getRewriter()),
             "void foo(){std::shared_ptr<int> a = std::shared_ptr<int>(new int[2]);}");
+}
+
+TEST_F(PtrRewriterTest, HandleClassesNoInit) {
+  tool.code = "class A {}; A *test(){A *a = new A; return a;}";
+
+  runToolOnCode(factory->create(), tool.code);
+  ASSERT_EQ(compareRewriterOutput(rewriter.getRewriter()),
+            "class A {}; std::shared_ptr<A> test(){std::shared_ptr<A> a = std::make_shared<A>(); return a;}");
+}
+
+TEST_F(PtrRewriterTest, HandleClassesCallInit) {
+  tool.code = "class A {}; A *test(){A *a = new A(); return a;}";
+
+  runToolOnCode(factory->create(), tool.code);
+  ASSERT_EQ(compareRewriterOutput(rewriter.getRewriter()),
+            "class A {}; std::shared_ptr<A> test(){std::shared_ptr<A> a = std::make_shared<A>(); return a;}");
+}
+
+TEST_F(PtrRewriterTest, HandleClassesListInit) {
+  tool.code = "class A {}; A *test(){A *a = new A{}; return a;}";
+
+  runToolOnCode(factory->create(), tool.code);
+  ASSERT_EQ(compareRewriterOutput(rewriter.getRewriter()),
+            "class A {}; std::shared_ptr<A> test(){std::shared_ptr<A> a = std::make_shared<A>(); return a;}");
+}
+
+TEST_F(PtrRewriterTest, HandleStructNoInit) {
+  tool.code = "struct A {}; A *test(){A *a = new A; return a;}";
+
+  runToolOnCode(factory->create(), tool.code);
+  ASSERT_EQ(compareRewriterOutput(rewriter.getRewriter()),
+            "struct A {}; std::shared_ptr<A> test(){std::shared_ptr<A> a = std::make_shared<A>(); return a;}");
+}
+
+TEST_F(PtrRewriterTest, HandleStructListInitValue) {
+  tool.code = "struct A {int m_a; A(int a) : m_a(a) {}}; A *test(){A *a = new A{1}; return a;}";
+
+  runToolOnCode(factory->create(), tool.code);
+  ASSERT_EQ(compareRewriterOutput(rewriter.getRewriter()),
+            "struct A {int m_a; A(int a) : m_a(a) {}}; std::shared_ptr<A> test(){std::shared_ptr<A> a = "
+            "std::make_shared<A>(1); return a;}");
+}
+
+TEST_F(PtrRewriterTest, HandleStructCallInitValue) {
+  tool.code = "struct A {int m_a; A(int a) : m_a(a) {}}; A *test(){A *a = new A(1); return a;}";
+
+  runToolOnCode(factory->create(), tool.code);
+  ASSERT_EQ(compareRewriterOutput(rewriter.getRewriter()),
+            "struct A {int m_a; A(int a) : m_a(a) {}}; std::shared_ptr<A> test(){std::shared_ptr<A> a = "
+            "std::make_shared<A>(1); return a;}");
 }
